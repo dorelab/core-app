9e3439d05e3ba0f85c1748efd93224429e515435
dorelab <dorelab@gmail.com>
2023-11-21 18:30:14 -0400

    Tabla de Votos en Calendarización

---
+++ src/app/dashboard/calendarizacion/components/table-votes/table-votes.component.ts
@@ -0,0 +1,27 @@
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
+import { IVotesIniciative } from '@app/shared';
+
+@Component({
+  selector: 'table-votes',
+  templateUrl: './table-votes.component.html',
+  styleUrls: ['./table-votes.component.scss'],
+})
+export class TableVotesComponent implements OnChanges {
+  @Input({ required: true }) dataSet: IVotesIniciative[] = [];
+
+  public displayedColumns = ['name', 'vote'];
+  public dataSource: IVotesIniciative[] = [];
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (this.dataSet) {
+      console.log(this.dataSet);
+      this.dataSource = this.dataSet;
+    }
+  }
+}
